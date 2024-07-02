0b10c1ac42331ab53e1475e9d0da9876b6999559
peperoxyz <deaanandagunawan@gmail.com>
2024-07-02 15:40:41 +0700

    add admin login feature

--- internal/controllers/authController.go
+++ internal/controllers/authController.go
@@ -41,3 +41,49 @@ func AdminRegister(ctx *gin.Context) {
 		"data": Admin,
 	})
 }
+
+func AdminLogin(ctx *gin.Context) {
+	db := database.GetDB()
+	contentType := helpers.GetContentType(ctx)
+	Admin := models.Admin{}
+	var password string
+
+	if contentType == "application/json" {
+		ctx.ShouldBindJSON(&Admin)
+	} else {
+		ctx.ShouldBind(&Admin)
+	}
+
+	password = Admin.Password // inputtan user
+
+	// find admin with inputted email
+	err := db.Debug().Where("email = ?", Admin.Email).Take(&Admin).Error
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H {
+			"error": "Unauthorized",
+			"message": "Invalid email",
+		})
+		return
+	}
+
+
+	// if email found, compare the hashed pass to the inputted pass
+	comparePass := helpers.ComparePass([]byte(Admin.Password), []byte(password))
+	if !comparePass {
+		ctx.JSON(http.StatusUnauthorized, gin.H {
+			"error": "Unauthorized",
+			"message": "Invalid password",
+		})
+		return
+	}
+
+	// if no problems found with the email and password, generate the token
+	token := helpers.GenerateToken(Admin.ID, Admin.UUID, Admin.Email)
+
+	ctx.JSON(http.StatusOK, gin.H {
+		"token": token,
+		"message": "Successfully logged in",
+	})
+
+
+}
