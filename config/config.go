43c27735dcd11f4f325612d1671863287b5c0094
peperoxyz <deaanandagunawan@gmail.com>
2024-07-05 00:28:01 +0700

    remove unused code

--- config/config.go
+++ config/config.go
@@ -3,17 +3,15 @@ package config
 import (
 	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 var SecretKey string
 
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	SecretKey = os.Getenv("SECRET_KEY")
 	if SecretKey == "" {
