31e018f97ae96d92c13b394b6bc994d1dc83b236
peperoxyz <deaanandagunawan@gmail.com>
2024-07-20 20:49:49 +0700

    comment unused code

--- config/config.go
+++ config/config.go
@@ -2,19 +2,20 @@ package config
 
 import (
 	"log"
-	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var SecretKey string
 
 func LoadEnv() {
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
-
-	SecretKey = os.Getenv("SECRET_KEY")
-	if SecretKey == "" {
-		log.Fatal("SECRET_KEY env variable not set")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
+
+	// SecretKey = os.Getenv("SECRET_KEY")
+	// if SecretKey == "" {
+	// 	log.Fatal("SECRET_KEY env variable not set")
+	// }
 }
